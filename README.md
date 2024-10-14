d693ac2767227e2fd8c5f643a42147ad27554ed5
Caleb Woodbine <calebwoodbine.public@gmail.com>
2024-10-15 12:11:10 +1300

    feat: initial commit
    first one

---
+++ README.md
@@ -0,0 +1,13 @@
+---
+display_name: Fedora Atomic KubeVirt VM
+description: Provision Fedora Atomic on KubeVirt VMs as Coder workspaces
+maintainer_github: ii
+verified: false
+tags: [kubernetes, vm, kubevirt, container]
+---
+
+# Create 
+
+``` sh
+coder template push kubevirt-vm
+```
---
+++ main.tf
@@ -0,0 +1,291 @@
+terraform {
+  required_providers {
+    coder = {
+      source = "coder/coder"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
+
+provider "coder" {
+}
+
+variable "use_kubeconfig" {
+  type        = bool
+  description = <<-EOF
+  Use host kubeconfig? (true/false)
+
+  Set this to false if the Coder host is itself running as a Pod on the same
+  Kubernetes cluster as you are deploying workspaces to.
+
+  Set this to true if the Coder host is running outside the Kubernetes cluster
+  for workspaces.  A valid "~/.kube/config" must be present on the Coder host.
+  EOF
+  default     = false
+}
+
+variable "namespace" {
+  type        = string
+  description = "The Kubernetes namespace to create workspaces in (must exist prior to creating workspaces). If the Coder host is itself running as a Pod on the same Kubernetes cluster as you are deploying workspaces to, set this to the same namespace."
+  default     = "default"
+}
+
+data "coder_parameter" "cpu" {
+  name         = "cpu"
+  display_name = "CPU"
+  description  = "The number of CPU cores"
+  default      = "2"
+  icon         = "/icon/memory.svg"
+  mutable      = true
+  option {
+    name  = "2 Cores"
+    value = "2"
+  }
+  option {
+    name  = "4 Cores"
+    value = "4"
+  }
+  option {
+    name  = "6 Cores"
+    value = "6"
+  }
+  option {
+    name  = "8 Cores"
+    value = "8"
+  }
+}
+
+data "coder_parameter" "memory" {
+  name         = "memory"
+  display_name = "Memory"
+  description  = "The amount of memory in GB"
+  default      = "2"
+  icon         = "/icon/memory.svg"
+  mutable      = true
+  option {
+    name  = "2 GB"
+    value = "2"
+  }
+  option {
+    name  = "4 GB"
+    value = "4"
+  }
+  option {
+    name  = "6 GB"
+    value = "6"
+  }
+  option {
+    name  = "8 GB"
+    value = "8"
+  }
+}
+
+data "coder_parameter" "home_disk_size" {
+  name         = "home_disk_size"
+  display_name = "Home disk size"
+  description  = "The size of the home disk in GB"
+  default      = "10"
+  type         = "number"
+  icon         = "/emojis/1f4be.png"
+  mutable      = false
+  validation {
+    min = 1
+    max = 99999
+  }
+}
+
+provider "kubernetes" {
+  # Authenticate via ~/.kube/config or a Coder-specific ServiceAccount, depending on admin preferences
+  config_path = var.use_kubeconfig == true ? "~/.kube/config" : null
+}
+
+data "coder_workspace" "me" {}
+data "coder_workspace_owner" "me" {}
+
+resource "coder_agent" "main" {
+  os             = "linux"
+  arch           = "amd64"
+  startup_script = <<-EOT
+    set -e
+
+    # Install the latest code-server.
+    # Append "--version x.x.x" to install a specific version of code-server.
+    curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server
+
+    # Start code-server in the background.
+    /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
+  EOT
+
+  # The following metadata blocks are optional. They are used to display
+  # information about your workspace in the dashboard. You can remove them
+  # if you don't want to display any information.
+  # For basic resources, you can use the `coder stat` command.
+  # If you need more control, you can write your own script.
+  metadata {
+    display_name = "CPU Usage"
+    key          = "0_cpu_usage"
+    script       = "coder stat cpu"
+    interval     = 10
+    timeout      = 1
+  }
+
+  metadata {
+    display_name = "RAM Usage"
+    key          = "1_ram_usage"
+    script       = "coder stat mem"
+    interval     = 10
+    timeout      = 1
+  }
+
+  metadata {
+    display_name = "Home Disk"
+    key          = "3_home_disk"
+    script       = "coder stat disk --path $${HOME}"
+    interval     = 60
+    timeout      = 1
+  }
+
+  metadata {
+    display_name = "CPU Usage (Host)"
+    key          = "4_cpu_usage_host"
+    script       = "coder stat cpu --host"
+    interval     = 10
+    timeout      = 1
+  }
+
+  metadata {
+    display_name = "Memory Usage (Host)"
+    key          = "5_mem_usage_host"
+    script       = "coder stat mem --host"
+    interval     = 10
+    timeout      = 1
+  }
+
+  metadata {
+    display_name = "Load Average (Host)"
+    key          = "6_load_host"
+    # get load avg scaled by number of cores
+    script   = <<EOT
+      echo "`cat /proc/loadavg | awk '{ print $1 }'` `nproc`" | awk '{ printf "%0.2f", $1/$2 }'
+    EOT
+    interval = 60
+    timeout  = 1
+  }
+}
+
+# code-server
+resource "coder_app" "code-server" {
+  agent_id     = coder_agent.main.id
+  slug         = "code-server"
+  display_name = "code-server"
+  icon         = "/icon/code.svg"
+  url          = "http://localhost:13337?folder=/home/coder"
+  subdomain    = false
+  share        = "owner"
+
+  healthcheck {
+    url       = "http://localhost:13337/healthz"
+    interval  = 3
+    threshold = 10
+  }
+}
+
+resource "kubernetes_secret" "userdata" {
+  count = data.coder_workspace.me.transition == "start" ? 1 : 0
+  metadata {
+    name      = "${data.coder_workspace.me.name}-userdata"
+    namespace = var.namespace
+  }
+  data = {
+    userdata = jsonencode(yamldecode(templatefile("cloud-config.yaml.tftpl", {
+      init_script       = urlencode(try(coder_agent.main.init_script, ""))
+      coder_agent_token = try(coder_agent.main.token, "")
+    })))
+  }
+}
+
+resource "kubernetes_manifest" "main" {
+  count = data.coder_workspace.me.transition == "start" ? 1 : 0
+  manifest = {
+    "apiVersion" = "kubevirt.io/v1"
+    "kind"       = "VirtualMachine"
+    "metadata" = {
+      "name"      = data.coder_workspace.me.name
+      "namespace" = var.namespace
+    }
+    "spec" = {
+      "running" = true
+      "template" = {
+        "metadata" = {
+          "creationTimestamp" = null
+          "labels" = {
+            "kubevirt.io/vm" = data.coder_workspace.me.name
+          }
+        }
+        "spec" = {
+          "domain" = {
+            "devices" = {
+              "disks" = [
+                {
+                  "disk" = {
+                    "bus" = "virtio"
+                  }
+                  "name" = "containerdisk"
+                },
+                {
+                  "disk" = {
+                    "bus" = "virtio"
+                  }
+                  "name" = "cloudinitdisk"
+                },
+              ]
+              "interfaces" = [
+                {
+                  "masquerade" = {}
+                  "name"       = "default"
+                },
+              ]
+              "rng" = {}
+            }
+            "resources" = {
+              "requests" = {
+                "memory" = "${data.coder_parameter.cpu.value}G"
+                "cpu"    = data.coder_parameter.memory.value
+              }
+            }
+          }
+          "networks" = [
+            {
+              "name" = "default"
+              "pod"  = {}
+            },
+          ]
+          "volumes" = [
+            {
+              "containerDisk" = {
+                "image" = "quay.io/fedora/fedora-coreos-kubevirt:stable"
+              }
+              "name" = "containerdisk"
+            },
+            {
+              "cloudInitConfigDrive" = {
+                "secretRef" = {
+                  "name" = kubernetes_secret.userdata[0].metadata[0].name
+                }
+              }
+              "name" = "cloudinitdisk"
+            },
+          ]
+        }
+      }
+    }
+  }
+
+  wait {
+    fields = {
+      "status.ready" = true
+    }
+  }
+}
